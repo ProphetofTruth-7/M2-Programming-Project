d738d1400bb8f476113faafcc469b64498e38209
Malakye Brown <prophetoftruth07@gmail.com>
2026-02-27 19:55:20 -0500

    Subproject Establishment

--- M2 Programming Project.cpp
+++ M2 Programming Project.cpp
@@ -1,10 +1,10 @@
-// This is the Main Branch. Delve into the sub-branches to grade
+// This is the Reverse Array Subproject! This program will fill an array of random size, then dynamically allocate an array that is filled with all elements in reverse
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "This is the MAIN BRANCH! All subprojects branch from here!" << endl;
+    cout << "This is the Reverse Array!" << endl;
     return 0;
 }
