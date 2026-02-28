cb8862bfec031637da843311c5543f130bb60912
Malakye Brown <prophetoftruth07@gmail.com>
2026-02-27 19:44:54 -0500

    Add project files.

---
+++ M2 Programming Project.cpp
@@ -0,0 +1,10 @@
+// This is the Main Branch. Delve into the sub-branches to grade
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This is the MAIN BRANCH! All subprojects branch from here!" << endl;
+    return 0;
+}
