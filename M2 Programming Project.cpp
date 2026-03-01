dc570ba32613d60ef1984d2a824bf683434b75e4
Malakye Brown <prophetoftruth07@gmail.com>
2026-03-01 13:00:34 -0500

    Initial Establishment

--- M2 Programming Project.cpp
+++ M2 Programming Project.cpp
@@ -1,10 +1,10 @@
-// This is the Main Branch. Delve into the sub-branches to grade
+// This is the Course Grade Subproject. It
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "This is the MAIN BRANCH! All subprojects branch from here!" << endl;
+    cout << "This is the Course Grade Subproject!" << endl;
     return 0;
 }
