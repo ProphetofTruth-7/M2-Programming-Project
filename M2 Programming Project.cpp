06046d0901479a735c29cd72de259ee3756ff69a
Malakye Brown <prophetoftruth07@gmail.com>
2026-03-01 14:11:52 -0500

    Help from Professor. Can't read HeaderFile from StudentData.txt into a const

--- M2 Programming Project.cpp
+++ M2 Programming Project.cpp
@@ -5,6 +5,14 @@
 #include <string>
 using namespace std;
 
+struct Student {
+    string name;
+    int ID;
+    //POINTER TO ARRAY
+    double averageScore;
+    char letterGrade;
+};
+
 int main()
 {
     string filename = "StudentData.txt"; //File Initialization bits
@@ -13,11 +21,20 @@ int main()
         cout << "Error opening file!" << endl;
         return 1;
     }
+    int intermediary;
 
-    string line;
-    getline(DATAFILE, line, ' ');
-    cout << line << endl;
-    
-    cout << "This is the Course Grade Subproject!" << endl;
+    DATAFILE >> intermediary;
+    int constexpr totalStudents = intermediary;
+    DATAFILE >> intermediary;
+    int constexpr totalGrades = intermediary;
+
+    cout << totalStudents + 5;
+
+    Student dataArray[totalStudents] = { {"Jordan", 26779, 55.4, 'F'}, { "Maya", 34303, 87.2, 'B' } };
+    cout << "Name: " << dataArray[0].name << " ID: " << dataArray[0].ID << endl;
+    cout << "Name: " << dataArray[1].name << " ID: " << dataArray[1].ID << endl;
+
+
+
     return 0;
 }
