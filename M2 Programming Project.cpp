8fcebc54c5ee14cf1e806d3a964af9f1686aae55
Malakye Brown <prophetoftruth07@gmail.com>
2026-03-01 13:30:14 -0500

    Checkpoint to verify Getline problems

--- M2 Programming Project.cpp
+++ M2 Programming Project.cpp
@@ -1,10 +1,23 @@
 // This is the Course Grade Subproject. It
 
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
+    string filename = "StudentData.txt"; //File Initialization bits
+    ifstream DATAFILE(filename);
+    if (!DATAFILE) {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(DATAFILE, line)) {
+        cout << line << endl;
+    }
+    
     cout << "This is the Course Grade Subproject!" << endl;
     return 0;
 }
